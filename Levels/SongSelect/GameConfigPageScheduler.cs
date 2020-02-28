042070ed39a5d8e333333f1c3dc9fe3984b00956
Kailang <kailangfu@gmail.com>
2020-02-28 00:28:38 -0600

    Add initial config panel

---
+++ Levels/SongSelect/GameConfigPageScheduler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Uif.Binding;
+
+namespace TouhouMix.Levels.SongSelect {
+	public sealed class GameConfigPageScheduler : TwoWayBindable {
+		float cacheTime_;
+		public float cacheTime {
+			get {return cacheTime_;}
+			set {cacheTime_ = value; BackPropagateValue("cacheTime", value);}
+		}
+
+		[ContextMenu("Reset Cache Time")]
+		public void dsf() {
+			cacheTime = 3;
+		}
+	}
+}
---
+++ Net/NetworkTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System;
+using System.Threading;
+using System.Text;
+using UnityEngine;
+
+public class NetworkTester : MonoBehaviour {
+	async void Start() {
+		var socket = new ClientWebSocket();
+//		socket.Options.AddSubProtocol("Tls");
+		var uri = new Uri("ws://localhost:1337");
+		await socket.ConnectAsync(uri, CancellationToken.None);
+
+		var bytesToSend = new ArraySegment<byte>(
+			Encoding.UTF8.GetBytes("hello fury from unity")
+		);
+		await socket.SendAsync(
+			bytesToSend, 
+			WebSocketMessageType.Text, 
+			true, 
+			CancellationToken.None
+		);
+	}
+}
